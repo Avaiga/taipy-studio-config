a4550a7644c0c4604654e907964f8492c8065014
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-01-11 12:12:35 +0700

    add license

--- src/providers/ConfigDetails.ts
+++ src/providers/ConfigDetails.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2022 Avaiga Private Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 import {
   WebviewViewProvider,
   WebviewView,
@@ -186,23 +199,23 @@ export class ConfigDetailsView implements WebviewViewProvider {
     // Use a nonce to only allow a specific script to be run.
     const nonce = getNonce();
     return `<html>
-				<head>
-					<meta charSet="utf-8"/>
-					<meta http-equiv="Content-Security-Policy" 
-								content="default-src 'none';
-								img-src vscode-resource: https:;
-								font-src ${webview.cspSource};
-								style-src ${webview.cspSource} 'unsafe-inline';
-								script-src ${getCspScriptSrc(nonce)};">             
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<link href="${styleUri}" rel="stylesheet" />
-					<link href="${codiconsUri}" rel="stylesheet" />
-					<script nonce="${nonce}" defer type="text/javascript" src="${scriptUri}"></script>
+                <head>
+                    <meta charSet="utf-8"/>
+                    <meta http-equiv="Content-Security-Policy" 
+                                content="default-src 'none';
+                                img-src vscode-resource: https:;
+                                font-src ${webview.cspSource};
+                                style-src ${webview.cspSource} 'unsafe-inline';
+                                script-src ${getCspScriptSrc(nonce)};">             
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link href="${styleUri}" rel="stylesheet" />
+                    <link href="${codiconsUri}" rel="stylesheet" />
+                    <script nonce="${nonce}" defer type="text/javascript" src="${scriptUri}"></script>
           <script nonce="${nonce}" type="text/javascript">window.taipyConfig=${JSON.stringify(getDefaultConfig(webview, this.extensionUri))};</script>
-				</head>
-				<body>
-					<div id="${containerId}"></div>
-				</body>
+                </head>
+                <body>
+                    <div id="${containerId}"></div>
+                </body>
       </html>`;
   }
 }
